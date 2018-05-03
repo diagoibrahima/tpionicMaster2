467fcd7e62157a62ad8d3b231e3cc4bb3ba1bdc0
El hadji Ibrahima DIAGO <elibrahimadiago@gmail.com>
2018-05-03 14:58:35 +0100

    App mobile isischool
    Premiere version de l'application

---
+++ isischool/src/pages/menu/menu.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, MenuController, Content  } from 'ionic-angular';
+
+/**
+ * Generated class for the MenuPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-menu',
+  templateUrl: 'menu.html',
+})
+export class MenuPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public menuCtrl: MenuController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MenuPage');
+  }
+
+  openMenu() {
+    this.menuCtrl.open();
+  }
+
+  closeMenu() {
+    this.menuCtrl.close();
+  }
+
+  toggleMenu() {
+    this.menuCtrl.toggle();
+  }
+
+}
